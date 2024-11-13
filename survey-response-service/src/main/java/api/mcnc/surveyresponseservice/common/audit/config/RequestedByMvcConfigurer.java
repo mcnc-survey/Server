b71c5021a9e7b3e215c06a3dc43342cdd96c93b6
UHeeJoon <yhjyhj3855@gmail.com>
2024-11-13 22:52:07 +0900

    feat: response-service에 jpa audit 추가

---
+++ survey-response-service/src/main/java/api/mcnc/surveyresponseservice/common/audit/config/RequestedByMvcConfigurer.java
@@ -0,0 +1,20 @@
+package api.mcnc.surveyresponseservice.common.audit.config;
+
+import api.mcnc.surveyresponseservice.common.audit.interceptor.RequestedByInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@RequiredArgsConstructor
+public class RequestedByMvcConfigurer implements WebMvcConfigurer {
+    private final RequestedByInterceptor requestedByInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addWebRequestInterceptor(requestedByInterceptor);
+    }
+}
