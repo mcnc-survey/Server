f90aec32ea36a57cff6c89d2608f869d36556bfa
chaikhyun <shetoo22@gmail.com>
2024-11-15 13:38:01 +0900

    feat: 이메일 html 발송 추가

--- email-service/src/main/java/api/mcnc/email_service/config/MailConfig.java
+++ email-service/src/main/java/api/mcnc/email_service/config/MailConfig.java
@@ -1,5 +1,6 @@
 package api.mcnc.email_service.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,13 +11,25 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${mail.host}")
+    private String host;
+
+    @Value("${mail.port}")
+    private Integer port;
+
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
     @Bean
     public JavaMailSender mailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("n5ybro2@gmail.com");
-        mailSender.setPassword("powaiewgmupfixcz");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
