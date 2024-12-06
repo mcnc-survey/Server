dc599ef8b912b1fe14a4ba08d110c4939382ff1e
UHeeJoon <yhjyhj3855@gmail.com>
2024-12-06 11:30:13 +0900

    feat: Long answer 타입, 조회시 좋아요 여부 확인

--- survey-service/src/main/java/api/mcnc/surveyservice/entity/question/QuestionType.java
+++ survey-service/src/main/java/api/mcnc/surveyservice/entity/question/QuestionType.java
@@ -7,5 +7,5 @@ package api.mcnc.surveyservice.entity.question;
  * @since :2024-11-13 오후 10:11
  */
 public enum QuestionType {
-  SINGLE_CHOICE, MULTIPLE_CHOICE, SHORT_ANSWER, TABLE_SELECT
+  SINGLE_CHOICE, MULTIPLE_CHOICE, SHORT_ANSWER, LONG_ANSWER, TABLE_SELECT
 }
