bcdc13e42135159c59762495592f026d9b609cc3
UHeeJoon <yhjyhj3855@gmail.com>
2024-12-11 14:19:34 +0900

    feat: 경로 수정

--- survey-service/src/main/java/api/mcnc/surveyservice/common/constants/SurveyLink.java
+++ survey-service/src/main/java/api/mcnc/surveyservice/common/constants/SurveyLink.java
@@ -7,5 +7,5 @@ package api.mcnc.surveyservice.common.constants;
  * @since :2024-11-27 오전 10:25
  */
 public class SurveyLink {
-  public static final String SURVEY_LINK = "https://mcnc-survey-client.vercel.app/mobile/survey";
+  public static final String SURVEY_LINK = "https://mcnc-survey-client.vercel.app/mobile?t=";
 }
