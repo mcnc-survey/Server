4e7f93da45b3b0aac6d300ca961fd57259dde989
UHeeJoon <yhjyhj3855@gmail.com>
2024-12-16 10:39:17 +0900

    feat: api test

---
+++ survey-admin-service/src/test/java/api/mcnc/surveyadminservice/RestDocsConfig.java
@@ -0,0 +1,25 @@
+package api.mcnc.surveyadminservice;
+
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+/**
+ * please explain class!
+ *
+ * @author :Uheejoon
+ * @since :2024-11-15 오전 10:11
+ */
+@TestConfiguration
+public class RestDocsConfig {
+
+  @Bean
+  public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcConfigurationCustomizer() {
+    return configurer -> configurer.operationPreprocessors()
+      .withRequestDefaults(prettyPrint())
+      .withResponseDefaults(prettyPrint());
+  }
+
+}
