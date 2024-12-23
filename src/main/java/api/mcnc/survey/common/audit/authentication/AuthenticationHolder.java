e4d13848f74ce5ed5606331e53a449c1e79a05a1
UHeeJoon <yhjyhj3855@gmail.com>
2024-12-23 16:14:27 +0900

    refactor: 안쓰는 폴더 제거

--- src/main/java/api/mcnc/survey/common/audit/authentication/AuthenticationHolder.java
+++
@@ -1,8 +0,0 @@
-package api.mcnc.survey.common.audit.authentication;
-
-import java.util.Optional;
-
-public interface AuthenticationHolder {
-    Optional<Authentication> getAuthentication();
-    void setAuthentication(Authentication authentication);
-}
