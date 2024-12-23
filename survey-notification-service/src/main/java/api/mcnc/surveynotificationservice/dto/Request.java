15e36aabf381e2c04ff30f3f9a3f102d33ed6228
chaikhyun <shetoo22@gmail.com>
2024-12-23 11:16:54 +0900

    feat: 알림dto수정

--- survey-notification-service/src/main/java/api/mcnc/surveynotificationservice/dto/Request.java
+++ survey-notification-service/src/main/java/api/mcnc/surveynotificationservice/dto/Request.java
@@ -1,7 +1,12 @@
 package api.mcnc.surveynotificationservice.dto;
 
+import api.mcnc.surveynotificationservice.constants.MsgFormat;
 import api.mcnc.surveynotificationservice.entity.NotificationEntity;
 import api.mcnc.surveynotificationservice.event.NotificationEvent;
+
+import java.util.Map;
+
+import static api.mcnc.surveynotificationservice.entity.NotificationEntity.Type.*;
 
 public record Request(
         String surveyId,
@@ -9,10 +14,27 @@ public record Request(
         NotificationEntity.Type type
 ) {
     public NotificationEvent toEvent(String adminId) {
+
+        Map<NotificationEntity.Type, String> messageMap = Map.of(
+                SURVEY_CREATE, MsgFormat.SURVEY_CREATE,
+                SURVEY_END, MsgFormat.SURVEY_END,
+                SURVEY_DELETE, MsgFormat.SURVEY_DELETE,
+                SURVEY_START, MsgFormat.SURVEY_START,
+                SURVEY_EDIT, MsgFormat.SURVEY_EDIT
+        );
+
+        // 기본 메시지 설정
+        String resMessage = messageMap.getOrDefault(type, "에러다 에러");
+
+        // 메시지 포맷 적용
+        if (!resMessage.equals("에러다 에러")) {
+            resMessage = String.format(resMessage, message);
+        }
+
         return NotificationEvent.builder()
                 .adminId(adminId)
                 .surveyId(surveyId)
-                .message(message)
+                .message(resMessage)
                 .type(type)
                 .build();
     }
